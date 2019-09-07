eb39f8b13772c967baf7ef22aef4d1f9d6a13603
coaiot <coaiot@tutanota.com>
2019-09-07 19:56:43 -0400

    Update chatbotscript.js

--- scripts/chatbotscript.js
+++ scripts/chatbotscript.js
@@ -1,7 +1,11 @@
+var flowStepCallback = function() {
+  console.log("CALLED")
+}
 window.onload = function() {
     var conversationalForm = window.cf.ConversationalForm.startTheConversation({
       formEl: document.getElementById("form"),
       context: document.getElementById("cf-context"),
+      flowStepCallback: flowStepCallback,
       submitCallback: function() {
        console.log("DONE")
       }
