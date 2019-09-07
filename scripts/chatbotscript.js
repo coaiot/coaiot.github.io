25a441f0712b2507f580c07b84d3a2f65784cf93
coaiot <coaiot@tutanota.com>
2019-09-07 14:07:49 -0400

    Tutorial Retry

--- scripts/chatbotscript.js
+++
@@ -1,9 +0,0 @@
-window.onload = function() {
-  var conversationalForm = window.cf.ConversationalForm.startTheConversation({
-    formEl: document.getElementById("form"),
-    context: document.getElementById("cf-context"),
-    submitCallback: function() {
-     conversationalForm.addRobotChatResponse("Alright, you are done."); 
-    }
-  });
-};
