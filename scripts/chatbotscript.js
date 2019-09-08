f65b86bd4239000bb4d20bae382f1a07e8233cdb
coaiot <coaiot@tutanota.com>
2019-09-07 20:14:27 -0400

    more flowcallback

--- scripts/chatbotscript.js
+++ scripts/chatbotscript.js
@@ -1,27 +1,7 @@
 var flowCallback = function(dto, success, error) {
   console.log("dto....", dto, success, error);
-
-  if (window.incrementedValue < 3) {
-    window.incrementedValue++;
-    window.ConversationalForm.addTags([
-      {
-        tag: "select",
-        name: "country",
-        "cf-questions":
-          "Where are you from? (I have asked you this " +
-          window.incrementedValue +
-          " time(s))",
-        "cf-input-placeholder": "country code",
-        multiple: false,
-        children: [
-          { tag: "option", "cf-label": "USA", value: "usa" },
-          { tag: "option", "cf-label": "UK", value: "uk" }
-        ]
-      }
-    ]);
-  }
-
-  // We assume every answer is valid. If that wasnt the case we would call error()
+  console.log("CALLED")
+  
   success();
 };
 
