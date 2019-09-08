a5799d1c2c7616f101a46b72af4fd88c422604cd
coaiot <coaiot@tutanota.com>
2019-09-07 20:11:57 -0400

    Update chatbotscript.js

--- scripts/chatbotscript.js
+++ scripts/chatbotscript.js
@@ -1,14 +1,36 @@
 var flowCallback = function(dto, success, error) {
-  console.log("CALLED")
-  success()
-}
+  console.log("dto....", dto, success, error);
+
+  if (window.incrementedValue < 3) {
+    window.incrementedValue++;
+    window.ConversationalForm.addTags([
+      {
+        tag: "select",
+        name: "country",
+        "cf-questions":
+          "Where are you from? (I have asked you this " +
+          window.incrementedValue +
+          " time(s))",
+        "cf-input-placeholder": "country code",
+        multiple: false,
+        children: [
+          { tag: "option", "cf-label": "USA", value: "usa" },
+          { tag: "option", "cf-label": "UK", value: "uk" }
+        ]
+      }
+    ]);
+  }
+
+  // We assume every answer is valid. If that wasnt the case we would call error()
+  success();
+};
 
 
 window.onload = function() {
     var conversationalForm = window.cf.ConversationalForm.startTheConversation({
       formEl: document.getElementById("form"),
       context: document.getElementById("cf-context"),
-      flowStepCallback: flowStepCallback,
+      flowStepCallback: flowCallback,
       submitCallback: function() {
        console.log("DONE")
       }
